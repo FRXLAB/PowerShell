c92427d73ca978e5fa7692e0c87bfab88b2b3643
FRXLAB <froux@frxlab.fr>
2025-10-22 15:25:59 +0200

    Mise à jour complète du projet

---
+++ prj1/FRX_Desktop.ps1
@@ -0,0 +1,16 @@
+﻿Function global:FRX_Desktop-Get-RecycleBinSize{
+	param()
+	(New-Object -ComObject Shell.Application).Namespace(0xA).Items() | ForEach-Object{$TailleCorbeille = 0}{
+        if($_.isfolder){$taillecorbeille += [int](dir $_.path -Recurse | measure -sum length).sum}
+        else           {$taillecorbeille += [int]$_.size}   
+    }
+    return $TailleCorbeille
+}
+
+Function global:FRX_Desktop-Flush-RecycleBin{
+    param()
+	(New-Object -ComObject Shell.Application).Namespace(0xA).Items() | ForEach-Object{
+        Remove-Item $_.Path -Recurse -Confirm:$false
+    }
+	return $null
+}
---
+++ prj2/FRX_CLICKER.ps1
@@ -0,0 +1,21 @@
+﻿
+function global:clic{
+    param()
+    $MouseEventSig=@' 
+[DllImport("user32.dll",CharSet=CharSet.Auto, CallingConvention=CallingConvention.StdCall)]
+public static extern void mouse_event(long dwFlags, long dx, long dy, long cButtons, long dwExtraInfo);
+'@ 
+$MouseEvent = Add-Type -memberDefinition $MouseEventSig -name "MouseEventWinApi" -passThru
+
+    $MouseEvent::mouse_event(0x00000002, 0, 0, 0, 0)
+    $MouseEvent::mouse_event(0x00000004, 0, 0, 0, 0)
+    return $null
+}
+function global:JustClic {
+    param($cpt=0)
+    while ($cpt -gt 0){
+        clic ; start-sleep -Milliseconds 100
+        $cpt--
+    }
+    return $null
+}
